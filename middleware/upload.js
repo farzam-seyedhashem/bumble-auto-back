41e03c2e586d429c599b3e1d5d18ab39181956e4
Frazam SeyedHashem <farzam.seyedhashem@gmail.com>
2021-02-06 16:00:34 +0330

    git ignore

---
+++ middleware/upload.js
@@ -0,0 +1,19 @@
+const util = require("util");
+const multer = require("multer");
+const maxSize = 2 * 1024 * 1024;
+let storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        console.log(__basedir)
+        cb(null, "./public/images/");
+    },
+    filename: (req, file, cb) => {
+        console.log(file);
+        cb(null, file.originalname);
+    },
+});
+let uploadFile = multer({
+    storage: storage,
+    // limits: { fileSize: maxSize },
+}).single("file");
+let uploadFileMiddleware = util.promisify(uploadFile);
+module.exports = uploadFileMiddleware;
---
+++ seeders/roles.seeder.js
@@ -0,0 +1,35 @@
+import {Seeder} from 'mongoose-data-seed';
+import Role from '../models/role_model';
+
+const data = [
+    {
+        name: 'admin',
+        accessibility: {
+            "service": {"get": true, "show": true, "post": true, "put": true, "delete": true},
+            "blog": {"get": true, "show": true, "post": true, "put": true, "delete": true},
+            "blog-category": {"get": true, "show": true, "post": true, "put": true, "delete": true},
+            "user": {"get": true, "show": true, "post": true, "put": true, "delete": true},
+            "language": {"get": true, "show": true, "post": true, "put": true, "delete": true},
+            "request": {"get": true, "show": true, "post": true, "put": true, "delete": true},
+            "role": {"get": true, "show": true, "post": true, "put": true, "delete": true},
+            "slider": {"get": true, "show": true, "post": true, "put": true, "delete": true},
+            "upload": {"get": true, "show": true, "post": true, "put": true, "delete": true},
+        }
+    },
+
+
+];
+
+class RolesSeeder extends Seeder {
+    async shouldRun() {
+        const count = await Role.countDocuments().exec();
+
+        return count === 0;
+    }
+
+    async run() {
+        return Role.create(data);
+    }
+}
+
+export default RolesSeeder;
